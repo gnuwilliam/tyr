f24479cdb5bce226c7ecee7556b69317a488941e
William Oliveira <sudowilliam@gmail.com>
2014-01-22 03:20:36 -0200

    adds more tests

--- test/simpleCalcTest.js
+++ test/simpleCalcTest.js
@@ -6,5 +6,13 @@ suite('simpleCalc', function () {
   test('1 + 1 should return 2', function () {
     assert.equal(2, simpleCalc(1, 1));
   });
-  
+
+  test('5 + 2 should return 7', function () {
+    assert.equal(7, simpleCalc(5, 2));
+  });
+
+  test('78 + 19 should return 97', function () {
+    assert.equal(97, simpleCalc(78, 19));
+  });
+
 });
