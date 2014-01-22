7e7ff7c7e75a7a2ae5de781782c4c2bc1100fef1
William Oliveira <sudowilliam@gmail.com>
2014-01-22 03:17:38 -0200

    change dir name

--- test/simpleCalcTest.js
+++ test/simpleCalcTest.js
@@ -1,8 +1,10 @@
 var assert = require("assert");
-var simple = require('./../simpleCalc/simpleCalc').simpleCalc;
+var simpleCalc = require('./../simple/simpleCalc').simpleCalc;
 
 suite('simpleCalc', function () {
+
   test('1 + 1 should return 2', function () {
     assert.equal(2, simpleCalc(1, 1));
   });
+  
 });
