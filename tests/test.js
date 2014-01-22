8e78565b47acc7da793ffee290f2f381151a1b66
William Oliveira <sudowilliam@gmail.com>
2014-01-22 03:12:51 -0200

    delete tests/

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-var assert = require("assert");
-var simple = require('./../simpleCalc/simpleCalc').simpleCalc;
-
-suite('simpleCalc', function () {
-  test('1 + 1 should return 2', function () {
-    assert.equal(2, simpleCalc(1, 1));
-  });
-});
